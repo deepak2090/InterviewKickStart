70476dd1ddbb6e167d559844e4a0b63bc422a15e
mohantymeenakshi23 <mohantymeenakshi23@gmail.com>
2023-03-05 20:10:27 -0500

    this is a commit of comments on logic

--- evenodd.py
+++ evenodd.py
@@ -4,6 +4,9 @@ from traceback import print_tb
 
 array = [5,6,1,8,33,9,10,55,12,47]
 
+#what the logic states is set evenptr at start I.E. is -1 and if
+#it finds a even number, increment the evenpointer and swap the element 
+#between I pointer and evenptr
 
 def InplaceEvenodd(array):
     evenptr = -1
