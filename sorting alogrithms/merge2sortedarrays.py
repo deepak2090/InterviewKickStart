db96e7715587a28699374a968ede8ce64138a48d
mohantymeenakshi23 <mohantymeenakshi23@gmail.com>
2023-06-19 10:19:16 -0400

    up

--- sorting alogrithms/merge2sortedarrays.py
+++ sorting alogrithms/merge2sortedarrays.py
@@ -2,7 +2,7 @@ first = [1, 3, 5]
 second = [2, 4, 6, 0, 0, 0]
 
 
-
+#inplace merged algorithm
 def merge_one_into_another(first, second):
     """
     Args:
@@ -33,3 +33,25 @@ def merge_one_into_another(first, second):
 
 
 print(merge_one_into_another(first, second))
+
+def merge2arrays(arr1, arr2):
+    merged = []
+    i = 0
+    j = 0
+    while i < len(arr1) and j <len(arr2):
+        if arr1[i] <= arr2[j]:
+            merged.append(arr1[i])
+            i +=1
+        else:
+            merged.append(arr2[j])
+            j+=1
+    while i < len(arr1):
+        merged.append(arr1[i])
+        i+=1
+    while j < len(arr2):
+        merged.append(arr2[j])
+        j+=1
+    return merged
+
+result = merge2arrays(array1,array2)
+print(result)
