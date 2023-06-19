41949b2263add202a098145fdc7ac9d1d2f2c8cc
mohantymeenakshi23 <mohantymeenakshi23@gmail.com>
2023-06-19 00:29:14 -0400

    update

---
+++ recurssion/subsets.py
@@ -0,0 +1,12 @@
+parenthesis = "xyz"
+def generate_all_subsets(s):
+    subset = ['']
+    for i in range(len(s)):
+        length = len(subset)
+        for j in range(length):
+            subset.append(subset[j] + s[i])
+    return subset
+
+print(generate_all_subsets(parenthesis))
+
+#learn tomorrow
