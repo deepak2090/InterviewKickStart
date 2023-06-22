51ee06c7391b4ad9985a4d5497207fa5f6e0e78d
mohantymeenakshi23 <mohantymeenakshi23@gmail.com>
2023-06-21 22:29:15 -0400

    update

---
+++ recurssion/Queen.py
@@ -0,0 +1,17 @@
+
+def square(n):
+    x = [['' for i in range(n)] for i in range(n)]
+    return x
+square(5)
+
+def queen(x):
+    for row in range(len(x)):
+        for col in range(len(x)):
+            if isvalid(x[row][col]):
+                x[row][col] == 'q'
+    return x 
+
+def isvalid(array[i][j]):
+    pass
+
+
