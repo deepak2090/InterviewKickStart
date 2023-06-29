f7dc9523487ee6b2880854fe567855f3bea22abf
mohantymeenakshi23 <mohantymeenakshi23@gmail.com>
2023-06-28 23:35:37 -0400

    update

--- recurssion/generalenumeration.py
+++ recurssion/generalenumeration.py
@@ -1,9 +1,54 @@
-list = ["d", "e", "e", "p", "a", "k"]
+string = "d1epak"
 
-def validcombination(list):
-    result = []
-    helper(list, len(list), slate)
+result = []
+def helper(s , i , slate):
+    if i == len(s):
+        result.append(slate)
+        return
+    if s[i].isdigit():
+        helper(s , i +1 , slate + s[i])
+    else:
+        helper(s, i+1, slate + s[i].swapcase())
+        helper(s, i+1, slate + s[i])
+
+def combinations(s):
+    slate = ""
+    helper(s, 0, slate)
     return result
 
-def helper(list, slate):
-    return slate
+print(combinations(string), len(result))
+
+
+
+def validateparenthesis(parenthesis):
+    output = 0
+    result = False
+    for i in range(len(parenthesis)):
+        if output < 0:
+            return False
+        if parenthesis[i] == '{':
+            output +=1
+        else:
+            output -=1
+    if output == 0:
+        return True
+
+def helper(s , i , slate):
+    if i == len(s):
+        if validateparenthesis(slate):
+            result.append(slate)
+        return
+    if s[i].isdigit():
+        helper(s , i +1 , slate + s[i])
+    else:
+        helper(s, i+1, slate + s[i].swapcase())
+        helper(s, i+1, slate + s[i])
+
+def combinations(s):
+    slate = ""
+    helper(s, 0, slate)
+    return result
+
+x = "{}{{}}"
+print(combinations(x), len(result))
+
