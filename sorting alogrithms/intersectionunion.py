2427cb5594e2b2e471fe073d1253c5a599b2ce15
mohantymeenakshi23 <mohantymeenakshi23@gmail.com>
2023-07-09 20:20:51 -0400

    update code and icons

--- sorting alogrithms/intersectionunion.py
+++ sorting alogrithms/intersectionunion.py
@@ -4,6 +4,8 @@ arr1 = [1,2,3,4,5,6,6,7,7,8,9,10,10]
 arr2 = [3,4,5,6,7,8,9,10,10,11,11]
 
 def intersection(array1,array2):
+    array1.sort()
+    array2.sort()
     i = 0
     j = 0
     result = []
@@ -12,13 +14,11 @@ def intersection(array1,array2):
             result.append(array1[i])
             i+=1
             j+=1
-        elif array1[i-1] == array1[i]:
-            i+=1
         elif array1[i] < array2[j]:
             i +=1
         else:
             j+=1
-        if i >1 and j >1:
+        if i >0 and j >0 and i < len(array1) and j <len(array2):
             if array1[i-1] == array1[i]:
                 i+=1
             if array2[j-1] == array2[j]:
