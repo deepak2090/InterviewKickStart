8b9d55198267b546d25a0ba4bc39a37ed698302e
mohantymeenakshi23 <mohantymeenakshi23@gmail.com>
2023-11-30 12:05:40 -0500

    update

--- leetcode/celery_flask/tasks.py
+++ leetcode/celery_flask/tasks.py
@@ -7,7 +7,7 @@ import logging
 app = Celery('myapp', broker='redis://localhost:6379/0',  backend='redis://localhost:6379/0')
 
 @app.task
-def say_hello():
+def say_hello(i):
     # Simulate a time-consuming operation (sleep for 10 seconds)
     #trigger_suit_cmd = ['pytest', '-v', '-s',  '--html', 'backgroundreport.html']
     os.chdir('/Users/deepakdas/InterviewKickStart/InterviewKickStart-1/leetcode/celery_flask/pytest')
@@ -16,7 +16,8 @@ def say_hello():
     stdout, stderr = process.communicate()
     logging.info(f"pytest output:\n{stdout.decode('utf-8')}")
     time.sleep(30)
-
+    print(f"the value is {i}", i)
+    logging.info(f"the value of the parameter is {i}") #this works and this is how parameter is passed
     # Check if the subprocess was successful
     if process.returncode != 0:
         logging.error(f"pytest subprocess failed with error: {stderr.decode('utf-8')}")
