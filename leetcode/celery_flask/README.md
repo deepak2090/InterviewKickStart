df1ef59057a3e3f280f927487519c4fb43b779a8
mohantymeenakshi23 <mohantymeenakshi23@gmail.com>
2023-12-01 18:58:38 -0500

    remove from azure function new

--- leetcode/celery_flask/README.md
+++
@@ -1,6 +0,0 @@
-#go inside directory and 
-#celery -A tasks worker --loglevel=info
-
-#start the flask
-
-#flask run  & export FLASK_APP=app
--- leetcode/flask_tutorial/04_03/url-shortener/test_main.py
+++
@@ -1,5 +0,0 @@
-from urlshort import create_app
-
-def test_shorten(client):
-    response = client.get('/')
-    assert b'Shorten' in response.data
