7f5bd42ea470d827fbeb04c67793720fa53a3b9b
mohantymeenakshi23 <mohantymeenakshi23@gmail.com>
2023-12-05 21:04:57 -0500

    update

--- leetcode/azurefnnew/function_app.py
+++ leetcode/azurefnnew/function_app.py
@@ -29,3 +29,4 @@ async def test_function(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
