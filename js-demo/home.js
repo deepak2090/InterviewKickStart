66814b8b25cd458c384abd0f14daf7d50c310778
mohantymeenakshi23 <mohantymeenakshi23@gmail.com>
2023-12-20 00:05:07 -0500

    update jsscript code

--- js-demo/home.js
+++ js-demo/home.js
@@ -41,3 +41,37 @@ seeReviewButton.addEventListener('click', function(){
 
     
 });
+
+const values = ['a','b','c'];
+console.log(values);
+let values1 = Array.of(1,2,3,4);
+values1.splice()
+
+const values2 = [1,2,3,4,5,6];
+const set1 = values2.filter(function(item){
+    return item >2;
+});
+console.log("the values are", set1);
+
+let app = {
+    "name": "deepak"
+}
+console.log(app.name)
+
+function Person(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.greet = function() {
+      console.log("Hello, " + this.firstName + " " + this.lastName + "!");
+    };
+  }
+
+// Creating an instance of the Person constructor
+var newPerson = new Person("John", "Doe", 30);
+
+// Accessing properties and calling a method
+console.log(newPerson.firstName);  // Output: "John"
+console.log(newPerson.age);        // Output: 30
+newPerson.greet();                 // Output: "Hello, John Doe!"
+
