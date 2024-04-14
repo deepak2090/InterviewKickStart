2cbb6d68f2bd1d3123e8c29610a0e25fcfc9fc1b
mohantymeenakshi23 <mohantymeenakshi23@gmail.com>
2024-04-14 10:05:17 -0400

    ss

--- sphinxtest/srccode/app.py
+++ sphinxtest/srccode/app.py
@@ -1,4 +1,7 @@
-def sum(a,b):
+from abc import abstractclassmethod, abstractmethod
+
+
+def sum(a: int,b: int) -> int:
     """
     :int a: number 1
     :int b: number 2
@@ -7,7 +10,8 @@ def sum(a,b):
     """
     return a +b
 
-def substraction(a,b):
+def substraction(a: int,b: int) -> float:
+    age: int =1
     """
     :int a: number 1
     :int b: number 2
