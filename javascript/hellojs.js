d3d163ba42eb470ceb2e614c3f54e2d09eef4ddf
mohantymeenakshi23 <mohantymeenakshi23@gmail.com>
2024-06-30 18:33:30 -0400

    up

--- javascript/hellojs.js
+++ javascript/hellojs.js
@@ -1,14 +1,5 @@
-// this is a comment!
-console.log('hello deepak');
-
-
-/*
-this
-is
-a
-multi-line
-comment
-*/
-
-/* this is a multi-line comment on a single line */
-
+//console.dir(window.document);
+console.dir(window.document);
+console.log(document.body.innerText);
+console.log(document.links[0]);
+//document.body.innerHTML = '<h1>this is from javascript</h1>'
